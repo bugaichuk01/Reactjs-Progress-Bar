47a1dcee6ae09bba5beed2529f5fe49b1d15cf07
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-20 16:07:04 +0300

    Main commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {ProgressBar} from "./components/ProgressBar";
 
 function App() {
+
+    const items = [
+        {name: 'Sold', color: '#BD1FBE', value: 677},
+        {name: 'Got free', color: '#FC64FF', value: 223},
+        {name: 'Burned', color: '#EA0000', value: 223},
+        {name: 'Free float', color: '#00ea5e', value: 800},
+        {name: 'Free float', color: '#00ea5e', value: 0},
+    ]
+
   return (
     <div className="App">
-     
+        <ProgressBar items={items} width={1400} height={25} />
+        <ProgressBar items={items} width={800} height={50} />
     </div>
   );
 }
---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import './ProgressBar.css';
+
+interface ProgressBarProps {
+    items: {
+        color: string;
+        value: number;
+        name: string
+    }[],
+    height: number;
+    width: number;
+}
+
+interface Item {
+    color: string;
+    value: number;
+    name: string
+}
+
+export const ProgressBar: React.FC<ProgressBarProps> = ({items, height, width}) => {
+
+    const sum = items.reduce((n, {value}) => n + value, 0);
+
+    return (
+        <div>
+            <div style={{maxWidth: `${width}px`}} className='container'>
+                <div className='progress-wrapper'>
+                    {items.map((item: Item) => (
+                        <div style={{width: `${Math.ceil((item.value * 100) / sum)}%`}}>
+                            {item.value > 0 && (
+                                <div>
+                                    <div className='progress'>
+                                        {[...Array(Math.ceil(60 / sum * item.value))].map((index) => (
+                                            <div className='progress-item' style={{backgroundColor: `${item.color}`, height: `${height}px`}}
+                                                 key={index}/>
+                                        ))}
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    ))}
+                </div>
+                <div className='info-wrapper'>
+                    {items.map((item: Item) => (
+                        item.value > 0 &&
+                        <span className='info'>
+                            <div style={{backgroundColor: `${item.color}`}} className='helper-color'/>
+                            <div className='desc'>{item.name} {item.value} ({((item.value * 100) / sum).toFixed(2)}%)</div>
+                        </span>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
