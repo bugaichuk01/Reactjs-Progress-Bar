212b504947e78cd6a5ae73d332ac1410291dd8a4
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-20 22:04:54 +0300

    Little Refactor

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -20,37 +20,27 @@ interface Item {
 export const ProgressBar: React.FC<ProgressBarProps> = ({items, height, width}) => {
 
     const sum = items.reduce((n, {value}) => n + value, 0);
+    const data = items.map((item) => ({
+        data: (Math.ceil((item.value / sum) * width / 25)) ,
+        ...item,
+    }));
+
+    const bars = data.map((item) =>
+      [...Array(Math.ceil(item.data))]
+          .map((index) => <div className='progress-item' style={{backgroundColor: `${item.color}`, height: `${height}px`}} key={index}/>))
 
     return (
-        <div>
-            <div style={{maxWidth: `${width}px`}} className='container'>
-                <div className='progress-wrapper'>
-                    {items.map((item: Item) => (
-                        <div>
-                            {item.value > 0 && (
-                                <div>
-                                    <div className='progress'>
-                                        {[...Array(Math.ceil((width / 35) / sum * item.value))].map((index) => (
-                                            <div className='progress-item'
-                                                 style={{backgroundColor: `${item.color}`, height: `${height}px`}}
-                                                 key={index}/>
-                                        ))}
-                                    </div>
-                                </div>
-                            )}
-                        </div>
-                    ))}
-                </div>
-                <div className={width >= 500 ? 'info-wrapper' : 'info-wrapper_column'}>
-                    {items.map((item: Item) => (
-                        item.value > 0 &&
-                        <span className='info'>
+        <div className='container' style={{maxWidth: `${width}px`}}>
+            <div className='progress'>{bars}</div>
+            <div className={width >= 500 ? 'info-wrapper' : 'info-wrapper__column'}>
+                {items.map((item: Item) => (
+                    item.value > 0 &&
+                    <span className='info'>
                             <div style={{backgroundColor: `${item.color}`}} className='helper-color'/>
                             <div
                                 className='desc'>{item.name} {item.value} ({((item.value * 100) / sum).toFixed(2)}%)</div>
                         </span>
-                    ))}
-                </div>
+                ))}
             </div>
         </div>
     );
